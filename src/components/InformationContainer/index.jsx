bd94fa9064918a973e230e47e35511e3c916e6fb
Pedro Mello <pedro.mello.rj@gmail.com>
2023-01-30 09:10:47 -0300

    feat: cria o componente de informações de contato para a tela de currículo + arquivo sass

---
+++ src/components/InformationContainer/index.jsx
@@ -0,0 +1,9 @@
+import '../../styles/components/informationContainer'
+
+function InformationContainer() {
+  return (
+    <div>InformationContainer</div>
+  )
+}
+
+export default InformationContainer
