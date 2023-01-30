398fdc0cd497f9b2afb0f6c83dcc6dbe2bf37a55
Pedro Mello <pedro.mello.rj@gmail.com>
2023-01-30 09:43:36 -0300

    feat: cria o componente "ProjectsContainer"

--- src/components/ProjectsContainer/index.jsx
+++ src/components/ProjectsContainer/index.jsx
@@ -1,6 +1,12 @@
 function ProjectsContainer() {
   return (
-    <div>ProjectsContainer</div>
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio sapiente minima reprehenderit vitae ducimus dolorum tempora iure obcaecati, necessitatibus aperiam, quo dignissimos assumenda! Ipsa autem, neque aliquam iste quos explicabo.</p>
+      <a href="" className="btn">
+        Ver Projetos
+      </a>
+    </section>
   )
 }
 
