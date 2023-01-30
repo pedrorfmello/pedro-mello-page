f8a8331faa0796f10799c2dec89c7eb33e705bb1
Pedro Mello <pedro.mello.rj@gmail.com>
2023-01-30 09:58:17 -0300

    feat: implementa o componente de tecnologias

--- src/components/TechnologiesContainer/index.jsx
+++ src/components/TechnologiesContainer/index.jsx
@@ -1,7 +1,38 @@
-function TechnologiesContainer() {
-  return (
-    <div>TechnologiesContainer</div>
-  )
+import {
+  DiHtml5,
+  DiCss3,
+  DiJsBadge,
+  DiMysql,
+  DiReact,
+  DiNodejsSmall,
+} from "react-icons/di";
+
+import "../../styles/components/technologiesContainer.sass";
+
+const TechnologiesContainer = () => {
+  const technologies = [
+    { id: "html", name: "HTML5", icon: <DiHtml5 /> },
+    { id: "css", name: "CSS3", icon: <DiCss3 /> },
+    { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
+    { id: "node", name: "Node. js", icon: <DiNodejsSmall /> },
+    { id: "mysql", name: "MYSQL", icon: <DiMysql /> },
+    { id: "react", name: "React", icon: <DiReact /> },
+  ];
+
+  return <section className="technologies-container">
+    <h2>Tecnologias</h2>
+    <div className="technologies-grid">
+      {technologies.map((tech) => (
+        <div className="technology-card" id={tech.id} key={tech.id}>
+          {tech.icon}
+          <div className="technology-info">
+            <h3>{tech.name}</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  </section>;
 }
 
-export default TechnologiesContainer
+export default TechnologiesContainer;
