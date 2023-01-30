ed34689b5bf3b64bc210446dedfc790f83830e40
Pedro Mello <pedro.mello.rj@gmail.com>
2023-01-30 09:28:21 -0300

    refact: componente "InformationContainer" consertando imports

--- src/components/InformationContainer/index.jsx
+++ src/components/InformationContainer/index.jsx
@@ -1,8 +1,32 @@
-import '../../styles/components/informationContainer'
+import { AiFillPhone, AiOutlineMail, AiFillEnvironment } from 'react-icons/ai'
+
+import '../../styles/components/informationContainer.sass'
 
 function InformationContainer() {
   return (
-    <div>InformationContainer</div>
+    <section id='information'>
+      <div className='info-card'>
+        <AiFillPhone id="phone-icon" />
+        <div>
+          <h3>Telefone</h3>
+          <p>(32) 9 9966-4687</p>
+        </div>
+      </div>
+      <div className='info-card'>
+        <AiOutlineMail id="email-icon" />
+        <div>
+          <h3>E-mail</h3>
+          <p>pedro.mello.rj@gmail.com</p>
+        </div>
+      </div>
+      <div className='info-card'>
+        <AiFillEnvironment id="pin-icon" />
+        <div>
+          <h3>Localização</h3>
+          <p>Cataguases - MG</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
