9b8c1af0f75f873da97316765769179d86be3bb0
Pedro Mello <pedro.mello.rj@gmail.com>
2023-01-30 09:41:25 -0300

    feat: adiciona o componente "AboutContainer"

--- src/components/AboutContainer/index.jsx
+++ src/components/AboutContainer/index.jsx
@@ -1,6 +1,10 @@
 function AboutContainer() {
   return (
-    <div>AboutContainer</div>
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam fuga possimus natus illo atque consequatur nobis ipsam, sunt iusto blanditiis, obcaecati eos vitae nisi dolore quasi? Consectetur beatae maxime neque.</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam fuga possimus natus illo atque consequatur nobis ipsam, sunt iusto blanditiis, obcaecati eos vitae nisi dolore quasi? Consectetur beatae maxime neque.</p>
+    </section>
   )
 }
 
