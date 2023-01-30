302a26fc9b2da731cc222f008ba6e61e1050fff4
Pedro Mello <pedro.mello.rj@gmail.com>
2023-01-30 09:43:48 -0300

    refact: altera a ordem das importações

--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -1,7 +1,8 @@
-import '../../styles/components/mainContent.sass';
 import AboutContainer from '../AboutContainer';
 import ProjectsContainer from '../ProjectsContainer';
 import TechnologiesContainer from '../TechnologiesContainer';
+
+import '../../styles/components/mainContent.sass';
 
 const MainContent = () => {
   return (
