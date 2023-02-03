5f5ee6dbe2c96ffe248130adf533943f93c20ecd
Pedro Mello <pedro.mello.rj@gmail.com>
2023-02-03 06:43:50 -0300

    feat: adiciona o link para download do curriculo

--- src/components/Curriculo/Sidebar/index.jsx
+++ src/components/Curriculo/Sidebar/index.jsx
@@ -1,17 +1,21 @@
-import SocialNetworks from '../SocialNetworks';
-import InformationContainer from '../InformationContainer';
-import Avatar from '../../../img/pedromello.jpg';
+import SocialNetworks from "../SocialNetworks";
+import InformationContainer from "../InformationContainer";
+import Avatar from "../../../img/pedromello.jpg";
 
-import '../../../styles/components/sidebar.sass';
+import "./../../../styles/components/sidebar.sass";
 
 const Sidebar = () => {
   return (
     <aside id="sidebar">
-      <img src={ Avatar } alt="Pedro Mello" />
+      <img src={Avatar} alt="Pedro Mello" />
       <p className="title">Desenvolvedor Front-End</p>
       <SocialNetworks />
       <InformationContainer />
-      <a href="" className="btn">
+      <a
+        href="https://drive.google.com/file/d/1hcDlVhTvs5k2ClzvVKxHrIogkMd-n-dk/view?usp=share_link"
+        target="_blank"
+        className="btn"
+      >
         Download Curriculo
       </a>
     </aside>
