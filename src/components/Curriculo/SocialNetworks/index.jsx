a97ad6dd52fd6e7ec60c1ee5f3ce4cbae48580f8
Pedro Mello <pedro.mello.rj@gmail.com>
2023-02-03 06:43:30 -0300

    feat: adiciona os links para as redes sociais

--- src/components/Curriculo/SocialNetworks/index.jsx
+++ src/components/Curriculo/SocialNetworks/index.jsx
@@ -1,23 +1,37 @@
-import { FaLinkedinIn, FaGithub, FaInstagram } from 'react-icons/fa';
+import { FaLinkedinIn, FaGithub, FaInstagram } from "react-icons/fa";
 
-import '../../../styles/components/socialNetworks.sass'
+import "./../../../styles/components/socialNetworks.sass";
 
 function SocialNetworks() {
   const socialNetworks = [
-    { name: 'linkedin', icon: <FaLinkedinIn /> },
-    { name: 'github', icon: <FaGithub /> },
-    { name: 'instagram', icon: <FaInstagram /> },
-  ]
+    {
+      name: "linkedin",
+      icon: <FaLinkedinIn />,
+      link: "https://www.linkedin.com/in/pedro-rfmello/",
+    },
+    {
+      name: "github",
+      icon: <FaGithub />,
+      link: "https://github.com/pedrorfmello",
+    },
+    { name: "instagram", icon: <FaInstagram />, link: "#" },
+  ];
 
   return (
     <section id="social-networks">
       {socialNetworks.map((network) => (
-        <a href="#" className="social-btn" id={network.name} key={network.name}>
+        <a
+          href={network.link}
+          className="social-btn"
+          target="_blank"
+          id={network.name}
+          key={network.name}
+        >
           {network.icon}
         </a>
       ))}
     </section>
-  )
+  );
 }
 
-export default SocialNetworks
+export default SocialNetworks;
